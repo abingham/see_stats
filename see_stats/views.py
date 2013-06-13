ad125f2c714c7581c80f981a08964281ecf189ef
Austin Bingham <austin.bingham@gmail.com>
2013-06-13 04:23:30 -0500

    Refactored commonality in response generation and templates.

--- see_stats/views.py
+++ see_stats/views.py
@@ -11,21 +11,36 @@ from pyramid.security import (authenticated_userid,
 from pyramid.view import view_config
 
 
+def common_response(request, rsp):
+    rslt = dict(rsp)
+    rslt.update(
+        dict(
+            favicon = request.static_url('see_stats:static/favicon.ico'),
+            stylesheet = request.static_url('see_stats:static/pylons.css'),
+            small_logo = request.static_url('see_stats:static/pyramid-small.png'),
+            userid = authenticated_userid(request),
+        ))
+    return rslt
+
 @view_config(route_name='upload', renderer='templates/upload.mustache')
 def upload(request):
-    return {
-        'userid': authenticated_userid(request),
-    }
+    return common_response(
+        request,
+        {
+            'userid': authenticated_userid(request),
+        })
 
 @view_config(route_name='profiles', renderer='templates/profiles.mustache')
 def profiles(request):
     userid = authenticated_userid(request)
     visible = lambda p: p['public'] or (p['userid'] == userid)
 
-    return {
-        'entries': list(filter(visible, request.db.profiles())),
-        'userid': userid,
-    }
+    return common_response(
+        request,
+        {
+            'entries': list(filter(visible, request.db.profiles())),
+            'userid': userid,
+        })
 
 @view_config(route_name='profile', renderer='templates/profile.mustache')
 def profile(request):
@@ -41,10 +56,12 @@ def profile(request):
         pstats.Stats(fname, stream=sio).print_stats()
 
     sio.seek(0)
-    return {
-        'stats': sio.read(),
-        'userid': authenticated_userid(request),
-    }
+    return common_response(
+        request,
+        {
+            'stats': sio.read(),
+            'userid': authenticated_userid(request),
+        })
 
 @view_config(route_name='process_upload')
 def process_upload(request):
@@ -87,19 +104,14 @@ def login(request):
                          headers=headers)
         # message = 'Failed login'
 
-    return dict(
-        message = message,
-        url = request.application_url + '/login',
-        came_from = came_from,
-        login = login,
-        # password = password,
-
-        # common stuff. TODO: Refactor?
-        favicon = request.static_url('see_stats:static/favicon.ico'),
-        stylesheet = request.static_url('see_stats:static/pylons.css'),
-        small_logo = request.static_url('see_stats:static/pyramid-small.png'),
-        userid = authenticated_userid(request),
-        )
+    return common_response(
+        request,
+        dict(
+            message = message,
+            url = request.application_url + '/login',
+            came_from = came_from,
+            login = login,
+        ))
 
 @view_config(route_name='logout')
 def logout(request):
