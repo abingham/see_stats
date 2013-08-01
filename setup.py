41ca360b6ead992259f85c5f1a71d075e37a7ff7
Austin Bingham <austin.bingham@gmail.com>
2013-08-01 18:32:16 +0200

    stuff

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import os
 
+from distribute_setup import use_setuptools
+use_setuptools()
+
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
+README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
